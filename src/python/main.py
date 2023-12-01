17327929e0b710f3c69fcaab2ba33bbb52f548b7
Matthieu Basto <matthieu.basto@gmail.com>
2023-12-01 12:04:24 +0100

    update python connections and commands

--- src/python/main.py
+++ src/python/main.py
@@ -1,15 +1,31 @@
+
+
 import mysql.connector
 
-connection_params = {
-    'host': "127.0.0.1",
-    'port':"8080",
-    'user': "root",
-    'password': "etibualuc",
-    'database': "test",
-}
+#establishing the connection
+conn = mysql.connector.connect(
+   user='user', password='userpwd', host='127.0.0.1', port='8080', database='db_first')
 
-with mysql.connector.connect(**connection_params) as db :
-    with db.cursor() as c:
-        print("connexion Successfull")
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
 
-        pass
+# Preparing SQL query to INSERT a record into the database.
+sql = """INSERT INTO Table_test(
+   name, email, region)
+   VALUES ('Matthieu Basto', 'matthieu.basto@gmail.com', 'Toulouse')"""
+
+try:
+   # Executing the SQL command
+   cursor.execute(sql)
+   print("try")
+   # Commit your changes in the database
+   conn.commit()
+
+except:
+   # Rolling back in case of error
+   print("exept")
+   conn.rollback()
+   conn.close()
+
+# Closing the connection
+conn.close()
