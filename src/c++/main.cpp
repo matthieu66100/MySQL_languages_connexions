6477b8897b60d8124a7f74d05019c412569ef8a7
Matthieu Basto <matthieu.basto@gmail.com>
2023-12-07 10:10:01 +0100

    first cmake test

--- src/c++/main.cpp
+++ src/c++/main.cpp
@@ -65,12 +65,10 @@ try {
 
 } catch (sql::SQLException &e) {
   cout << "# ERR: SQLException in " << __FILE__;
-  cout << "(" << __FUNCTION__ << ") on line " »
-     << __LINE__ << endl;
+  cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
   cout << "# ERR: " << e.what();
   cout << " (MySQL error code: " << e.getErrorCode();
-  cout << ", SQLState: " << e.getSQLState() << »
-     " )" << endl;
+  cout << ", SQLState: " << e.getSQLState() << " )" << endl;
 }
 
 cout << endl;
